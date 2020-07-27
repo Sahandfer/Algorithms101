877cae35f677f0d0f4fcc99023bd280c012c8784
Sahand Sabour <karimiali0022@yahoo.com>
2020-07-27 17:42:31 +0800

    :check_mark Updated Main

--- Sorting Algorithms/Main.dart
+++ Sorting Algorithms/Main.dart
@@ -2,6 +2,7 @@ import 'Insertion_Sort.dart';
 import 'Selection_Sort.dart';
 import 'Merge_Sort.dart';
 import 'Quick_Sort.dart';
+import 'Bubble_sort.dart';
 
 void main(List<String> args) {
   var arr = [3, 6, 4, 5, 2, 1, 7, 0, 9, 8];
@@ -21,7 +22,12 @@ void main(List<String> args) {
       break;
 
     case 'Quick':
+      quick_sort(arr);
+      break;
+    case 'Bubble':
+      bubble_sort(arr);
       break;
     default:
+      print("Lol! Don't know that algorithm yet...");
   }
 }
